b55e8d58d093949be73aabc1636d91ae7ddd8b02
Edwardhsu <edwardhsu@localhost.localdomain>
2019-09-05 02:58:07 -0400

    init db with table

--- mysql_file/init.sql
+++ mysql_file/init.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE iii_project;
+CREATE DATABASE IF NOT EXISTS iii_project CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE iii_project;
+CREATE TABLE eur_usd_4hr(
+    DateTime   DATETIME      PRIMARY KEY,
+    Open   DECIMAL(10,5) NOT NULL,
+    High   DECIMAL(10,5) NOT NULL,
+    Low    DECIMAL(10,5) NOT NULL,
+    Close  DECIMAL(10,5) NOT NULL);
